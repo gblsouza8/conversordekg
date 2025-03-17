b24a755ed36d96f31b7dd54dd4bed841ce7b6749
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-17 13:49:36 -0300

    Segundo Commit, testando o básico do programa

---
+++ conversor.py
@@ -0,0 +1,6 @@
+# Programa que converte KG para as outras unidades de medidas de peso...
+
+valor =  int(input("Insira aqui o valor (em kg) que você deseja converter: "))
+
+grama = valor*1000
+print(grama)
