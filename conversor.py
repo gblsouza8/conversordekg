2a2710a4cf5a8ac778dac107602e247e0c35e71c
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-17 14:02:59 -0300

    Terceiro Commit

--- conversor.py
+++ conversor.py
@@ -1,6 +1,37 @@
 # Programa que converte KG para as outras unidades de medidas de peso...
 
-valor =  int(input("Insira aqui o valor (em kg) que você deseja converter: "))
+def conversor(valor):
 
-grama = valor*1000
-print(grama)
+    # Converte para as outras unidades de medidas
+    grama = valor*1000
+    miligrama = valor*1e+6
+    stone = valor/6.35
+    libra = valor*2.205
+    onca = valor*35.274
+
+    # Exibe o resultado
+    print(valor, "kg (s) convertido para:\nGramas:", grama, "\nMiligramas:", miligrama, "\nStone:", stone, "\nLibras:", libra, "\nOnça:", onca);
+    rodarNovamente()
+    
+    
+    
+ # Pede ao usuário para inserir o valor que deseja converter (em kg)
+ 
+def __init__():
+    valor =  int(input("Insira aqui o valor (em kg) que você deseja converter: "))
+    conversor(valor)
+    
+def rodarNovamente():
+        escolha = int(input("Deseja converter outro valor?\n1. Sim\n2. Não\n"))
+        if (escolha == 1):
+            print("Iniciando o programa novamente...")
+            __init__()
+        elif (escolha == 2):
+            print("Tudo bem! finalizando o programa...")
+            exit()
+        else:
+            print("Escolha inválida... por favor, escolha uma opção válida.")
+            rodarNovamente()
+            
+            
+__init__()
